9126c590f77c3f50f134f265b65afaf5ea0badc4
Joel Fernandez <joelfernandez.relg@gmail.com>
2025-06-14 17:01:46 -0300

    Add bootstrap en static y directorios para views

--- grupo24oo2/src/main/java/com/unla/grupo24oo2/helpers/ViewRouterHelper.java
+++ grupo24oo2/src/main/java/com/unla/grupo24oo2/helpers/ViewRouterHelper.java
@@ -1,16 +1,16 @@
 package com.unla.grupo24oo2.helpers;
 
 public class ViewRouterHelper {
-	public static final String HOME = "home";
-	public static final String REGISTRO_EXITOSO = "registro_exitoso";
-	public static final String REGISTRO = "registro";
-	public static final String FORMULARIO_TICKET = "formulario_ticket";
-	public static final String CREACION_TICKET = "creacion_ticket_exitosa";
-	public static final String REGISTRO_EMPLEADO = "registro_empleado";
-	public static final String HISTORIAL_TICKET = "historial_ticket";
-	public static final String LOGIN = "login";
-	public static final String FORMULARIO_INTERVENCION = "formulario_intervencion";
-	public static final String CREACION_INTERVENCION = "creacion_intervencion_exitosa";
-	public static final String ASOCIADOS_TICKET = "ticket_asociados";
-	public static final String MIS_DATOS = "mis_datos";
+	public static final String HOME = "home/home";
+	public static final String REGISTRO_EXITOSO = "registro/registro_exitoso";
+	public static final String REGISTRO = "registro/registro";
+	public static final String FORMULARIO_TICKET = "ticket/formulario_ticket";
+	public static final String CREACION_TICKET = "ticket/creacion_ticket_exitosa";
+	public static final String REGISTRO_EMPLEADO = "registro/registro_empleado";
+	public static final String HISTORIAL_TICKET = "ticket/historial_ticket";
+	public static final String LOGIN = "login/login";
+	public static final String FORMULARIO_INTERVENCION = "intervencion/formulario_intervencion";
+	public static final String CREACION_INTERVENCION = "intervencion/creacion_intervencion_exitosa";
+	public static final String ASOCIADOS_TICKET = "ticket/ticket_asociados";
+	public static final String MIS_DATOS = "perfil/mis_datos";
 }
